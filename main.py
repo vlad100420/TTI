cef1fefe5034a571ce750b26cdfdf31645d4a4ce
vlad100420 <95189881+vlad100420@users.noreply.github.com>
2022-01-28 16:35:50 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import Problema1 as p1
+import Problema2 as p2
+import Problema3 as p3
+S = ['s1', 's2', 's3', 's4', 's5', 's6']
+P = [0.30, 0.10, 0.05, 0.25, 0.20, 0.10]
+
+print(p1.p1(S, P))
+print(p2.p1(S, P))
+print(p3.p1("ana are mere"))
+#print(dic_code)
