78beabc228aa1b3f2812a9df06fa4df5b706397e
vlad100420 <95189881+vlad100420@users.noreply.github.com>
2022-01-28 16:32:39 +0200

    Delete Problema4.py

--- Problema4.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def vector_in_matrice(matrice: np.ndarray, vector: np.ndarray) -> bool:
-    for i in matrice:
-        if np.array_equal(i, vector):
-            return True
-            break
-    return False
