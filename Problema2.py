1dc2827a64329196e2ba06fc7ad5c7791525d799
vlad100420 <95189881+vlad100420@users.noreply.github.com>
2022-01-28 16:27:14 +0200

    Delete Problema2.py

--- Problema2.py
+++
@@ -1,8 +0,0 @@
-def gaseste_maxim_aparitii(lista: list) -> tuple:
-    res = max(set(lista), key=lista.count)
-    count = 0
-    for i in lista:
-        if res == i:
-            count += 1
-    pereche = res, count
-    return pereche
